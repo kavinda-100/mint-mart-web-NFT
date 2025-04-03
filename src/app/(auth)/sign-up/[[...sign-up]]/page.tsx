fec7cfd15e4355e001a4c7f2af483e7cc3dac565
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-03 20:20:44 +0530

    feat: implement authentication check in SignIn and SignUp pages

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = await auth();
+  if (userId) {
+    return redirect("/");
+  }
   return <SignUp />;
 }
