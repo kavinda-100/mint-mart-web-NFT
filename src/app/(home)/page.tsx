9c3cd0f7bcea775b04d84f0cf18e8bb643a6f196
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-03 12:10:56 +0530

    feat: add Radix UI button component and update dependencies

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../../components/ui/button";
+
 export default function HomePage() {
   return (
     <section>
       <h1 className={"mt-10 text-center text-3xl font-bold"}>Mint Mart</h1>
+      <Button>Go to Marketplace</Button>
     </section>
   );
 }
