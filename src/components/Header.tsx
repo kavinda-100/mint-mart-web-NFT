eaacce3475b5325fd826e544362fe98e1ba462ce
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-03 18:47:36 +0530

    feat: add logo and gradient text styling to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <header
-      className={
-        "fixed top-0 right-0 left-0 container mx-auto h-10 bg-blue-400"
-      }
-    >
-      Header
+    <header className={"fixed top-0 right-0 left-0 container mx-auto h-10"}>
+      <div className={"flex justify-between gap-3"}>
+        {/* Logo */}
+        <div className={"flex items-center gap-2"}>
+          <Image src={"/logo.png"} alt={"logo"} width={50} height={50} />
+          <h1 className={"logo-text"}>Mint Mart</h1>
+        </div>
+      </div>
     </header>
   );
 };
