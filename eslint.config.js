5c0b1fc31e9006cb070efae481513402e36ebf2a
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-03 11:41:11 +0530

    refactor: disable additional TypeScript ESLint rules for improved flexibility

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,8 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
         { prefer: "type-imports", fixStyle: "inline-type-imports" },
