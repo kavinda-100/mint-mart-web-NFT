9a7b86e165ab943b4d4d62d8f9758a1ef3fbc87e
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-05 11:08:45 +0530

    feat: update HomeLayout styles for improved full-height layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header";
 
 const HomeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <main className={"container mx-auto flex flex-col"}>
+    <main className={"flex h-full w-full flex-col"}>
       <Header />
       <section className={"mt-[50px] min-h-screen w-full"}>{children}</section>
     </main>
