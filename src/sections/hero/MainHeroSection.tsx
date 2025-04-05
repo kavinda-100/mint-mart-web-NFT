bf936ddc99029894546243d56540f2dff5f19653
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-05 09:57:45 +0530

    feat: enhance MainHeroSection with gradient button and improved layout

--- src/sections/hero/MainHeroSection.tsx
+++ src/sections/hero/MainHeroSection.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Image from "next/image";
+import { Button } from "../../components/ui/button";
 
 const MainHeroSection = () => {
   return (
-    <div className={"container mx-auto grid h-screen grid-cols-3"}>
+    <div className={"container mx-auto grid h-screen grid-cols-3 gap-5"}>
       {/* section one */}
       <div className={"col-span-3 flex flex-col gap-4 lg:col-span-1"}>
         <Image
           src={"/hero-one.png"}
           alt={"NFT Image"}
-          width={500}
-          height={500}
+          width={400}
+          height={400}
         />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          tristique, nunc ut bibendum tincidunt, nisi nunc aliquet nunc, eget
+          aliquam nunc nunc eget nunc. Sed tristique, nunc ut bibendum
+          tincidunt, nisi nunc aliquet nunc, eget aliquam nunc nunc eget nunc.
+        </p>
+        <Button variant={"gradient"}>Buy Now</Button>
       </div>
 
       {/* section two */}
