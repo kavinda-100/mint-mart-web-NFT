061b825afca0d7896eaa7ae7ceb5f5993d4524a1
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-05 10:37:39 +0530

    feat: add SecondHeroSection component and integrate it into HomePage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,12 @@
 import MainHeroSection from "../../sections/hero/MainHeroSection";
+import SecondHeroSection from "../../sections/hero/SecondHeroSection";
 
 export default function HomePage() {
   return (
     <section className={"container mx-auto"}>
+      {/*  Hero section */}
       <MainHeroSection />
+      <SecondHeroSection />
     </section>
   );
 }
---
+++ src/sections/hero/SecondHeroSection.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "../../components/ui/button";
+import Link from "next/link";
+
+const SecondHeroSection = () => {
+  return (
+    <div className={"container mx-auto grid h-screen grid-cols-3 gap-5"}>
+      {/* section one */}
+      <div
+        className={"col-span-3 flex flex-col gap-4 px-2 lg:col-span-1 lg:px-0"}
+      >
+        <Image
+          src={"/hero-one.png"}
+          alt={"NFT Image"}
+          width={400}
+          height={400}
+        />
+        <p className={"text-base font-bold text-pretty lg:mb-3"}>
+          Welcome to the future of digital ownership! Our NFT marketplace is the
+          ultimate destination for creators, collectors, and investors.
+        </p>
+        <Button variant={"gradient"} asChild>
+          <Link href={"/browse"}>Buy Now</Link>
+        </Button>
+      </div>
+
+      {/* section two */}
+      <div
+        className={
+          "col-span-3 flex h-full w-full flex-col items-center justify-center px-2 lg:col-span-2 lg:ml-5 lg:px-0"
+        }
+      >
+        <h1 className={"text-7xl font-extrabold text-pretty lg:text-9xl"}>
+          Start Making Money With NFT&apos;s{" "}
+          <div className={"h-2 w-full bg-white"} />
+        </h1>
+      </div>
+    </div>
+  );
+};
+export default SecondHeroSection;
