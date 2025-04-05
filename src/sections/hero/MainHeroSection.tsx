c9ca93276a2af3425e1b134cc758e7c3cfc3c25f
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-05 10:36:25 +0530

    feat: enhance MainHeroSection layout with improved spacing and responsive design

--- src/sections/hero/MainHeroSection.tsx
+++ src/sections/hero/MainHeroSection.tsx
@@ -7,14 +7,16 @@ const MainHeroSection = () => {
   return (
     <div className={"container mx-auto grid h-screen grid-cols-3 gap-5"}>
       {/* section one */}
-      <div className={"col-span-3 flex flex-col gap-4 lg:col-span-1"}>
+      <div
+        className={"col-span-3 flex flex-col gap-4 px-2 lg:col-span-1 lg:px-0"}
+      >
         <Image
           src={"/hero-one.png"}
           alt={"NFT Image"}
           width={400}
           height={400}
         />
-        <p className={"text-base font-bold text-pretty"}>
+        <p className={"text-base font-bold text-pretty lg:mb-3"}>
           Welcome to the future of digital ownership! Our NFT marketplace is the
           ultimate destination for creators, collectors, and investors.
         </p>
@@ -26,10 +28,10 @@ const MainHeroSection = () => {
       {/* section two */}
       <div
         className={
-          "col-span-3 flex h-full w-full flex-col items-center justify-center lg:col-span-2 lg:ml-5"
+          "col-span-3 flex h-full w-full flex-col items-center justify-center px-2 lg:col-span-2 lg:ml-5 lg:px-0"
         }
       >
-        <h1 className={"text-4xl font-extrabold text-pretty lg:text-9xl"}>
+        <h1 className={"text-7xl font-extrabold text-pretty lg:text-9xl"}>
           Start Making Money With NFT&apos;s{" "}
           <div className={"h-2 w-full bg-white"} />
         </h1>
